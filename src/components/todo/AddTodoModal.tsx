22be895eb8d62df14337493bff01459662d3439c
sreemaye <sreemaye44@gmail.com>
2024-01-14 12:31:43 +0600

    dispatch taskdetails

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -12,14 +12,21 @@ import { Label } from "@radix-ui/react-dropdown-menu";
 import { Input } from "../ui/input";
 import { FormEvent, useState } from "react";
 import { DialogClose } from "@radix-ui/react-dialog";
+import { useAppDispatch } from "@/redux/hook";
+import { addTodo } from "@/redux/features/todoSlice";
 const AddTodoModal = () => {
 
     const [task, setTask] = useState('');
     const [description, setDescription] = useState('');
+    const dispatch = useAppDispatch()
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
-        console.log({ task, description })
+        const taskDetails = {
+            title: task,
+            description
+        }
+        dispatch(addTodo(taskDetails))
     }
 
     return (
